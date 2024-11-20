956c8b256469c9d5d246fbcafee54a9f3ce6a6ba
James Goppert <james.goppert@gmail.com>
2024-11-20 00:10:43 -0500

    Update build.rs
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- build.rs
+++ build.rs
@@ -2,5 +2,6 @@ use rerun_except::rerun_except;
 
 fn main() {
     lalrpop::process_root().unwrap();
-    rerun_except(&["src/generators/templates/*.tera"]).unwrap();
+    rerun_except(&["templates/*.tera"]).unwrap();
+    rerun_except(&["models/*.mo"]).unwrap();
 }
