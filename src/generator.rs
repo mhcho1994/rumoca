ffc6209c5b7515d44df39de073f0c8b7f5078b21
James Goppert <james.goppert@gmail.com>
2025-01-02 20:10:34 -0500

    Improve parser error user feedback.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- src/generator.rs
+++ src/generator.rs
@@ -5,6 +5,7 @@ use crate::parser;
 use crate::tokens::{LexicalError, Token};
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::process;
 
 use codespan_reporting::diagnostic::{Diagnostic, Label};
 use codespan_reporting::files::SimpleFiles;
@@ -19,67 +20,51 @@ pub fn parse_file(
     filename: &str,
 ) -> Result<ast::StoredDefinition, ParseError<usize, Token, LexicalError>> {
     let mut files = SimpleFiles::new();
-    let file_id = files.add(filename, std::fs::read_to_string(filename).unwrap());
-    let file = files.get(file_id).expect("failed to get file");
+    let file_id = files.add(filename, std::fs::read_to_string(filename).expect("failed to read file"));
+    let file = files.get(file_id).expect("failed to get file id");
     let lexer = Lexer::new(file.source());
     let parser = StoredDefinitionParser::new();
     let def = parser.parse(lexer);
     if def.is_err() {
-        // let type_id = def.as_ref().unwrap().type_id();
-        // match type_id {
-        //     UnrecognizedToken => println!("unrecognized token!"),
-        //     _ => println!("type unhandled {:?}", type_id.)
-        // }
         let err = def.as_ref().expect_err("error");
-
         let writer = StandardStream::stderr(ColorChoice::Always);
         let config = codespan_reporting::term::Config::default();
 
         match err {
             ParseError::User { error } => match error {
                 LexicalError::InvalidInteger(e) => {
-                    println!("lexer invalid integer:{}", e);
+                    println!("lexer invalid integer:{:?}", e);
                 }
                 LexicalError::InvalidToken => {
                     println!("lexer invalid token {:?}", error);
                 }
             },
             ParseError::InvalidToken { location } => {
-                println!("invalid token loc:{}", location);
+                println!("invalid token loc:{:?}", location);
             }
             ParseError::ExtraToken { token } => {
                 println!("extra token: {:?}", token);
             }
             ParseError::UnrecognizedEof { location, expected } => {
-                println!("unrecognized Eof loc: {}, expected:", location);
+                println!("unrecognized Eof loc: {:?}, expected:", location);
                 for tok in expected {
-                    println!("{}", tok)
+                    println!("{:?}", tok)
                 }
             }
             ParseError::UnrecognizedToken { token, expected } => {
-                println!("unrecognized token {:?}, expected:", token);
-                for tok in expected {
-                    println!("expected: {}", tok)
-                }
-                let diagonistic = Diagnostic::error()
-                    .with_message("failed to parse")
+                let diagonistic = Diagnostic::error().with_message("Unrecognized Token")
                     .with_code("E001")
                     .with_labels(vec![
                         Label::primary(file_id, (token.0)..(token.2)),
                         Label::secondary(file_id, (0)..(token.2)),
                     ])
-                    .with_notes(vec![
-                        expected[0].clone(),
-                        unindent::unindent(
-                            "
-                            expected type \"=\"
-                        ",
-                        ),
-                    ]);
+                    .with_notes(vec!["expected one of: ".to_string(), expected.join(", ")]);
                 codespan_reporting::term::emit(&mut writer.lock(), &config, &files, &diagonistic)
                     .expect("fail");
             }
         }
+        // kill process to avoid panicing when parse fails, codespan already reports error
+        process::exit(1);
     }
     def
 }
--- src/tokens.rs
+++ src/tokens.rs
@@ -250,6 +250,6 @@ pub enum Token {
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self)
+        write!(f, "{:?}", self)
     }
 }
